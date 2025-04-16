d2d788efe41831bc4631fccac0e4e68a64b08632
jTawadros <jtawadros.tc@gmail.com>
2025-04-16 12:32:32 -0700

    Add deepseek bot

--- creativeblock/app/_layout.tsx
+++ creativeblock/app/_layout.tsx
@@ -1,12 +1,19 @@
 import { Stack } from 'expo-router';
+import ChatBubble from '@/components/ChatBubble';
 
 export default function RootLayout() {
-    return (
-        <Stack>
-            <Stack.Screen name="index" options={{ title: 'Home' }} />
-            <Stack.Screen name="createproject" options={{ title: 'Create Project' }} />
-            <Stack.Screen name="projecthome" options={{ title: 'Project Home' }} />
-            <Stack.Screen name="projectview" options={{ title: 'Project View' }} />
-        </Stack>
-    );
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: 'Home' }} />
+        <Stack.Screen name="createproject" options={{ title: 'Create Project' }} />
+        <Stack.Screen name="projecthome" options={{ title: 'Project Home' }} />
+        <Stack.Screen name="projectview" options={{ title: 'Project View' }} />
+      </Stack>
+
+      {/* Add the floating chat bubble */}
+      <ChatBubble />
+    </>
+  );
 }
+
