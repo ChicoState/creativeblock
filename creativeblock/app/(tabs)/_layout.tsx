ad87377fd9f79ce010301e86886e093b764f92b2
David Wilson <dkwilson1@csuchico.edu>
2025-04-30 12:47:25 -0700

    Dark Mode tabs

--- creativeblock/app/(tabs)/_layout.tsx
+++ creativeblock/app/(tabs)/_layout.tsx
@@ -1,9 +1,26 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { useThemeColor } from '@/hooks/useThemeColor';
 
-export default () =>{
+export default () => {
+    const backgroundColor = useThemeColor({}, 'background'); 
+    const textColor = useThemeColor({}, 'text');
+    const tintColor = useThemeColor({}, 'tint'); 
   return (
-   <Tabs>
+      <Tabs screenOptions={{
+          headerStyle: {
+              backgroundColor: backgroundColor,
+          },
+          headerTintColor: textColor,
+          headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20
+          },
+          tabBarActiveBackgroundColor: backgroundColor,
+          tabBarInactiveTintColor: textColor,
+          tabBarActiveTintColor: textColor,
+          tabBarInactiveBackgroundColor: backgroundColor,
+      }}>
     <Tabs.Screen name = "home" />
     <Tabs.Screen name = "My Projects" />
     <Tabs.Screen name = "popular" />
